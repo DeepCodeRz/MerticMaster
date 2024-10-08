962643d25f15c356df8d6a2c76a5ce83a4e127ac
DeepoweRs <bedirhan_kurt_@outlook.com>
2024-10-08 14:51:08 +0200

    0.2 final update
    result variables assignet

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ class Measure {
 const convertBtn = document.getElementById("convertBtn");
 
 convertBtn.addEventListener("click", () => {
-    const convertQuantity = document.getElementById("input").value;
+    const convertQuantity = parseFloat(document.getElementById("input").value);
     const measure = new Measure(convertQuantity);
     const values = [3.281, 0.264, 2.204]
     let results = []
@@ -21,7 +21,10 @@ convertBtn.addEventListener("click", () => {
     }
 
     const outputs = document.getElementsByClassName("output");
-    outputs[0].innerText = `${measure.quantity} meters = ${results[0]} feet | ${measure.quantity} feet = ${results[1]} meters`;
-    outputs[1].innerText = `${measure.quantity} liters = ${results[2]} gallons | ${measure.quantity} gallons = ${results[3]} liters`;
-    outputs[2].innerText = `${measure.quantity} kilos = ${results[4]} pounds | ${measure.quantity} pounds = ${results[5]} kilos`;
+    const [feet, meter, gallon, liter, pound, kilo] = results
+    const [length, volume, mass] = outputs
+
+    length.innerText = `${measure.quantity} meters = ${feet} feet | ${measure.quantity} feet = ${meter} meters`;
+    volume.innerText = `${measure.quantity} liters = ${gallon} gallons | ${measure.quantity} gallons = ${liter} liters`;
+    mass.innerText = `${measure.quantity} kilos = ${pound} pounds | ${measure.quantity} pounds = ${kilo} kilos`;
 })
